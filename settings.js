f3ccb505f4291f72b30c7a752544906b4e489611
zephyr523 <140008097+zephyr523@users.noreply.github.com>
2024-11-01 14:14:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,12 +7,13 @@ global.autoblockmorroco = true //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
 global.antispam = false//auto kick spammer (true to on, false to off)
 //===============SETTING MENU==================\\
-function _0x6e24(){var _0x5c78f5=['13209250uMnVNa','3oTJXuL','GitHub:\x20Itzpatron','andana','ownername','1492MRNTav','11209143JsVlSJ','48tsodMR','ᴘᴀᴛʀᴏɴ-ᴍᴅ','botname','Nigeria','socialm','ownernomer','angopay','438LQLrsL','ownernumber','2348133729715','nodana','PATRON\x20🚹','168zUqXCN','justt.patron','nogopay','15295wOcmNS','owner','FADARE','location','2179244aHbiKK','843yVMciv','42235tLHQMg','./data/image/thumb.jpg','8133729715','10889415ZFociw'];_0x6e24=function(){return _0x5c78f5;};return _0x6e24();}var _0x58159b=_0x168f;function _0x168f(_0x2db1ad,_0x500e0b){var _0x6e2460=_0x6e24();return _0x168f=function(_0x168fff,_0x10b160){_0x168fff=_0x168fff-0x121;var _0xd54aaa=_0x6e2460[_0x168fff];return _0xd54aaa;},_0x168f(_0x2db1ad,_0x500e0b);}(function(_0x385124,_0x533608){var _0x2cc83a=_0x168f,_0x3ece05=_0x385124();while(!![]){try{var _0xa524b2=parseInt(_0x2cc83a(0x138))/0x1*(-parseInt(_0x2cc83a(0x122))/0x2)+-parseInt(_0x2cc83a(0x13e))/0x3*(-parseInt(_0x2cc83a(0x137))/0x4)+parseInt(_0x2cc83a(0x139))/0x5*(-parseInt(_0x2cc83a(0x12b))/0x6)+-parseInt(_0x2cc83a(0x133))/0x7*(parseInt(_0x2cc83a(0x130))/0x8)+-parseInt(_0x2cc83a(0x13c))/0x9+-parseInt(_0x2cc83a(0x13d))/0xa+parseInt(_0x2cc83a(0x123))/0xb*(parseInt(_0x2cc83a(0x124))/0xc);if(_0xa524b2===_0x533608)break;else _0x3ece05['push'](_0x3ece05['shift']());}catch(_0x20afa6){_0x3ece05['push'](_0x3ece05['shift']());}}}(_0x6e24,0xc2f91),global['thumbnail']=fs['readFileSync'](_0x58159b(0x13a)),global['ig']=_0x58159b(0x131),global['yt']='Itzpatron1',global['ttowner']=_0x58159b(0x131),global[_0x58159b(0x121)]=_0x58159b(0x12f),global[_0x58159b(0x134)]=[_0x58159b(0x12d)],global[_0x58159b(0x129)]=_0x58159b(0x12d),global[_0x58159b(0x128)]=_0x58159b(0x13f),global[_0x58159b(0x136)]=_0x58159b(0x127),global[_0x58159b(0x12e)]='8133729715',global[_0x58159b(0x132)]='8133729715',global['noovo']=_0x58159b(0x13b),global[_0x58159b(0x140)]=_0x58159b(0x135),global[_0x58159b(0x12a)]=_0x58159b(0x135),global['anovo']=_0x58159b(0x135),global[_0x58159b(0x126)]=_0x58159b(0x125),global[_0x58159b(0x12c)]=_0x58159b(0x12d));
-var _0x4746bf=_0x148d;function _0x148d(_0x2b6c77,_0x83b2f6){var _0x2337fb=_0x2337();return _0x148d=function(_0x148d13,_0x4e7b65){_0x148d13=_0x148d13-0x11e;var _0x1c948e=_0x2337fb[_0x148d13];return _0x1c948e;},_0x148d(_0x2b6c77,_0x83b2f6);}function _0x2337(){var _0x41f0c5=['561896TpdndG','author','2348133729715@s.whatsapp.net','1iHSTbK','84iaPbEu','wagc','1081076EhLbVk','227616AmSLUk','ownerNumber','botscript','10Avvgco','4nLFWrG','ᴘᴀᴛʀᴏɴ-ᴍᴅ🚹','https://github.com/Itzpatron/PATRON-MD','3039765UxuMjB','455754TopaZc','ᴘᴀᴛʀᴏɴ-ᴍᴅ','Sticker\x20created\x20by','ᴘᴀᴛʀᴏɴ-ᴍᴅ\x20🚹','20802199deYjhP','ownername','https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q','5033331EAqKxB','ownerweb'];_0x2337=function(){return _0x41f0c5;};return _0x2337();}(function(_0x4519e8,_0x31b9b9){var _0x5f3a31=_0x148d,_0x34aea7=_0x4519e8();while(!![]){try{var _0xb572bf=parseInt(_0x5f3a31(0x126))/0x1*(parseInt(_0x5f3a31(0x129))/0x2)+parseInt(_0x5f3a31(0x12a))/0x3*(parseInt(_0x5f3a31(0x12e))/0x4)+-parseInt(_0x5f3a31(0x131))/0x5+parseInt(_0x5f3a31(0x132))/0x6*(-parseInt(_0x5f3a31(0x127))/0x7)+parseInt(_0x5f3a31(0x123))/0x8+-parseInt(_0x5f3a31(0x121))/0x9+parseInt(_0x5f3a31(0x12d))/0xa*(parseInt(_0x5f3a31(0x11e))/0xb);if(_0xb572bf===_0x31b9b9)break;else _0x34aea7['push'](_0x34aea7['shift']());}catch(_0x399d66){_0x34aea7['push'](_0x34aea7['shift']());}}}(_0x2337,0x79d5c),global[_0x4746bf(0x11f)]=_0x4746bf(0x12f),global[_0x4746bf(0x12b)]=['2348133729715@s.whatsapp.net'],global[_0x4746bf(0x122)]=_0x4746bf(0x120),global['websitex']=_0x4746bf(0x120),global[_0x4746bf(0x128)]=_0x4746bf(0x120),global['saluran']=_0x4746bf(0x120),global['themeemoji']='🚹',global['wm']=_0x4746bf(0x133),global[_0x4746bf(0x12c)]=_0x4746bf(0x130),global['packname']=_0x4746bf(0x134),global[_0x4746bf(0x124)]=_0x4746bf(0x135),global['creator']=_0x4746bf(0x125));
-global.botnumber = '2348133729715'
-global.bankname = "MONIEPOINT"
-global.banknumber = "8133729715"
-global.bankowner = "FADARE"
+function _0x6e24(){var _0x5c78f5=['13209250uMnVNa','3oTJXuL','GitHub:\x20zephyr523','andana','ownername','1492MRNTav','11209143JsVlSJ','48tsodMR','𝕬𝖗𝖈𝖍𝖔𝖓ᴹᴰ-ᴮᴼᵀ ','botname','Nigeria','socialm','ownernomer','angopay','438LQLrsL','ownernumber',2347059751569','nodana','𝕬𝖗𝖈𝖍𝖔𝖓\x20🦇','168zUqXCN','𝚟𝚎𝚜𝚙𝚎𝚛','nogopay','15295wOcmNS','owner','𓂀 𝔗𝔥𝔢 𝕍𝔢𝔰𝔭𝔢𝔯𓂀','location','2179244aHbiKK','843yVMciv','42235tLHQMg','./data/image/thumb.jpg','8133729715','10889415ZFociw'];_0x6e24=function(){return _0x5c78f5;};return _0x6e24();}var _0x58159b=_0x168f;function _0x168f(_0x2db1ad,_0x500e0b){var _0x6e2460=_0x6e24();return _0x168f=function(_0x168fff,_0x10b160){_0x168fff=_0x168fff-0x121;var _0xd54aaa=_0x6e2460[_0x168fff];return _0xd54aaa;},_0x168f(_0x2db1ad,_0x500e0b);}(function(_0x385124,_0x533608){var _0x2cc83a=_0x168f,_0x3ece05=_0x385124();while(!![]){try{var _0xa524b2=parseInt(_0x2cc83a(0x138))/0x1*(-parseInt(_0x2cc83a(0x122))/0x2)+-parseInt(_0x2cc83a(0x13e))/0x3*(-parseInt(_0x2cc83a(0x137))/0x4)+parseInt(_0x2cc83a(0x139))/0x5*(-parseInt(_0x2cc83a(0x12b))/0x6)+-parseInt(_0x2cc83a(0x133))/0x7*(parseInt(_0x2cc83a(0x130))/0x8)+-parseInt(_0x2cc83a(0x13c))/0x9+-parseInt(_0x2cc83a(0x13d))/0xa+parseInt(_0x2cc83a(0x123))/0xb*(parseInt(_0x2cc83a(0x124))/0xc);if(_0xa524b2===_0x533608)break;else _0x3ece05['push'](_0x3ece05['shift']());}catch(_0x20afa6){_0x3ece05['push'](_0x3ece05['shift']());}}}(_0x6e24,0xc2f91),global['thumbnail']=fs['readFileSync'](_0x58159b(0x13a)),global['ig']=_0x58159b(0x131),global['yt']='Itzpatron1',global['ttowner']=_0x58159b(0x131),global[_0x58159b(0x121)]=_0x58159b(0x12f),global[_0x58159b(0x134)]=[_0x58159b(0x12d)],global[_0x58159b(0x129)]=_0x58159b(0x12d),global[_0x58159b(0x128)]=_0x58159b(0x13f),global[_0x58159b(0x136)]=_0x58159b(0x127),global[_0x58159b(0x12e)]='8133729715',global[_0x58159b(0x132)]='8133729715',global['noovo']=_0x58159b(0x13b),global[_0x58159b(0x140)]=_0x58159b(0x135),global[_0x58159b(0x12a)]=_0x58159b(0x135),global['anovo']=_0x58159b(0x135),global[_0x58159b(0x126)]=_0x58159b(0x125),global[_0x58159b(0x12c)]=_0x58159b(0x12d));
+var _0x4746bf=_0x148d;function _0x148d(_0x2b6c77,_0x83b2f6){var _0x2337fb=_0x2337();return _0x148d=function(_0x148d13,_0x4e7b65){_0x148d13=_0x148d13-0x11e;var _0x1c948e=_0x2337fb[_0x148d13];return _0x1c948e;},_0x148d(_0x2b6c77,_0x83b2f6);}function _0x2337(){var _0x41f0c5=['561896TpdndG','author','2347059751569@s.whatsapp.net','1iHSTbK','84iaPbEu','wagc','1081076EhLbVk','227616AmSLUk','ownerNumber','botscript','10Avvgco','4nLFWrG','𝕬𝖗𝖈𝖍𝖔𝖓ᴹᴰ-ᴮᴼᵀ ','https://github.com/zephyr523/PATRON-MD','3039765UxuMjB','455754TopaZc','𝕬𝖗𝖈𝖍𝖔𝖓ᴹᴰ-ᴮᴼᵀ ','Sticker\x20created\x20by','𝕬𝖗𝖈𝖍𝖔𝖓ᴹᴰ-ᴮᴼᵀ \x20🦇','20802199deYjhP','ownername','https://whatsapp.com/channel/0029VapKm2f23n3ehnzErQ1m','5033331EAqKxB','ownerweb'];_0x2337=function(){return _0x41f0c5;};return _0x2337();}(function(_0x4519e8,_0x31b9b9){var _0x5f3a31=_0x148d,_0x34aea7=_0x4519e8();while(!![]){try{var _0xb572bf=parseInt(_0x5f3a31(0x126))/0x1*(parseInt(_0x5f3a31(0x129))/0x2)+parseInt(_0x5f3a31(0x12a))/0x3*(parseInt(_0x5f3a31(0x12e))/0x4)+-parseInt(_0x5f3a31(0x131))/0x5+parseInt(_0x5f3a31(0x132))/0x6*(-parseInt(_0x5f3a31(0x127))/0x7)+parseInt(_0x5f3a31(0x123))/0x8+-parseInt(_0x5f3a31(0x121))/0x9+parseInt(_0x5f3a31(0x12d))/0xa*(parseInt(_0x5f3a31(0x11e))/0xb);if(_0xb572bf===_0x31b9b9)break;else _0x34aea7['push'](_0x34aea7['shift']());}catch(_0x399d66){_0x34aea7['push'](_0x34aea7['shift']());}}}(_0x2337,0x79d5c),global[_0x4746bf(0x11f)]=_0x4746bf(0x12f),global[_0x4746bf(0x12b)]=['2347059751569@s.whatsapp.net'],global[_0x4746bf(0x122)]=_0x4746bf(0x120),global['websitex']=_0x4746bf(0x120),global[_0x4746bf(0x128)]=_0x4746bf(0x120),global['saluran']=_0x4746bf(0x120),global['themeemoji']='🦇',global['wm']=_0x4746bf(0x133),global[_0x4746bf(0x12c)]=_0x4746bf(0x130),global['packname']=_0x4746bf(0x134),global[_0x4746bf(0x124)]=_0x4746bf(0x135),global['creator']=_0x4746bf(0x125));
+global.botnumber = '2347059751569'
+global.bankname = "OPAY"
+global.banknumber = "8054729374"
+global.bankowner = "ZEPHYRUS BLAKE"
+global.bankaccountname = " EMILY ISRAEL"
 //======================== CPANEL COMMAND ===========================\\
 global.domain = '-' // Fill in your domain, don't put a / at the end of the link
 global.apikey = '-' // Fill Apikey
@@ -84,12 +85,12 @@ global.rpg = {
             trash: '🗑',
             armor: '🥼',
             sword: '⚔️',
-            makanancentaur: "🥗",
-            makanangriffin: "🥙",
-            makanankyubi: "🍗",
-            makanannaga: "🍖",
-            makananpet: "🥩",
-            makananphonix: "🧀",
+            foodunicorn: "🥗",
+            foodgiraffe: "🥙",
+            foodfox: "🍗",
+            fooddragon: "🍖",
+            foodpet: "🥩",
+            foodeagle: "🧀",
             pickaxe: '⛏️',
             fishingrod: '🎣',
             wood: '🪵',
@@ -105,29 +106,29 @@ global.rpg = {
             gold: '🪙',
             emerald: '❇️',
             upgrader: '🧰',
-            bibitanggur: '🌱',
-            bibitjeruk: '🌿',
-            bibitapel: '☘️',
-            bibitmangga: '🍀',
-            bibitpisang: '🌴',
-            anggur: '🍇',
-            jeruk: '🍊',
-            apel: '🍎',
-            mangga: '🥭',
-            pisang: '🍌',
-            botol: '🍾',
-            kardus: '📦',
-            kaleng: '🏮',
-            plastik: '📜',
-            gelas: '🧋',
+            grapeseed: '🌱',
+            orangeseed: '🌿',
+            appleseed: '☘️',
+            mangoseed: '🍀',
+            bananastem: '🌴',
+            grape: '🍇',
+            orange: '🍊',
+            apple: '🍎',
+            mango: '🥭',
+            banana: '🍌',
+            bottle: '🍾',
+            box: '📦',
+            cans: '🏮',
+            plastic: '📜',
+            glass: '🧋',
             chip: '♋',
-            umpan: '🪱',
-            naga: "🐉",
-            phonix: "🦅",
-            kyubi: "🦊",
-            griffin: "🦒",
-            centaur: "🎠",
-            skata: '🧩'
+            snake: '🪱',
+            dragon: "🐉",
+            eagle: "🦅",
+            fox: "🦊",
+            giraffe: "🦒",
+            unicorn: "🎠",
+            magpie: '🧩'
         }
         let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
         if (!results.length) return ''
@@ -154,24 +155,24 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 
 //messages
 global.mess = {
-wait: "*_ᴘᴀᴛʀᴏɴ🚹, ᴡᴀɪᴛ!!!._*",
-   success: "ʜᴇʜᴇ, *ɪᴛ ᴡᴀꜱ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ🚹*",
-   on: "ɪᴛ'ꜱ ᴀᴄᴛɪᴠᴇ🚹", 
-   off: "ɪᴛ'ꜱ ᴏꜰꜰ ꜱɪʀ🚹",
+wait: "*_𝕬𝖗𝖈𝖍𝖔𝖓ᴹᴰ 𝚙𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐..._*",
+   success: "*𝚄𝚜𝚎𝚛 𝚛𝚎𝚚𝚞𝚎𝚜𝚝𝚜𝚞𝚌𝚌𝚎𝚜𝚜𝚏𝚞𝚕𝚕𝚢 𝚌𝚊𝚛𝚛𝚒𝚎𝚍 𝚘𝚞𝚝*🦇",
+   on: "𝙸𝚝'𝚜 𝚊𝚌𝚝𝚒𝚟𝚎🦇", 
+   off: "𝙸𝚝 𝚑𝚊𝚜 𝚋𝚎𝚎𝚗 𝚍𝚎𝚊𝚌𝚝𝚒𝚟𝚊𝚝𝚎𝚍🦇",
    query: {
-       text: "ᴡʜᴇʀᴇ ɪꜱ ᴛʜᴇ ᴛᴇxᴛ ʙʀᴏ?🚹",
-       link: "ᴡʜᴇʀᴇ'ꜱ ᴛʜᴇ ʟɪɴᴋ?🚹",
+       text: "𝚃𝚎𝚡𝚝 𝚞𝚗𝚒𝚍𝚎𝚗𝚝𝚒𝚏𝚒𝚎𝚍!!🦇",
+       link: "𝙻𝚒𝚗𝚔 𝚄𝚗𝚒𝚍𝚎𝚗𝚝𝚒𝚏𝚒𝚎𝚍!!🦇",
    },
    error: {
-       fitur: "ʏᴏᴏ ꜱᴏʀʀʏ, ᴇʀʀᴏʀ ꜰᴇᴀᴛᴜʀᴇ, ᴘʟᴇᴀꜱᴇ ᴄʜᴀᴛ ᴡɪᴛʜ ᴛʜᴇ ʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ ꜱᴏ ɪᴛ ᴄᴀɴ ʙᴇ ꜰɪxᴇᴅ ɪᴍᴍᴇᴅɪᴀᴛᴇʟʏ. 🚹",
+       fitur: "𝙴𝚁𝚁𝙾𝚁!!.... 𝙵𝙴𝙰𝚃𝚄𝚁𝙴 𝚂𝙴𝙴𝙼𝚂 𝚃𝙾 𝙱𝙴 𝙵𝙰𝚄𝙻𝚃𝚈 𝙿𝙻𝙴𝙰𝚂𝙴 𝙲𝙷𝙰𝚃 𝚆𝙸𝚃𝙷 𝚃𝙷𝙴 𝙱𝙾𝚃 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 𝚂𝙾 𝙸𝚃 𝙲𝙰𝙽 𝙱𝙴 𝙵𝙸𝚇𝙴𝙳. 🦇",
    },
    only: {
-       group: "ʏᴏᴏ ᴍᴀɴ, ʏᴏᴜ ᴄʀᴀᴢʏ? ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜꜱᴇᴅ ɪɴ ɢʀᴏᴜᴘꜱ. 🚹",
-       private: "ʙʀᴜʜ ᴡᴛꜰ. ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜꜱᴇᴅ ɪɴ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ. 🚹",
-       owner: "ʏᴏᴏ ʙʀᴏ ɢᴛꜰ. ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜꜱᴇᴅ ʙʏ ʙᴏᴛ ᴏᴡɴᴇʀꜱ. 🚹",
-       admin: "ʏᴏᴏ ʙʀᴏ ɢᴛꜰ. ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜꜱᴇᴅ ʙʏ ʙᴏᴛ ᴏᴡɴᴇʀꜱ. 🚹",
-       badmin: "ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ꜰɪʀꜱᴛ ɴɪɢɢᴀ. 🚹",
-       premium: "ɴɪɢɢᴀ ᴡᴛꜰ ʏᴏᴜ ᴀɪɴ'ᴛ ᴍʏ ᴏᴡɴᴇʀ. 🚹",
+       group: " 𝚃𝙷𝙸𝚂 𝙵𝙴𝙰𝚃𝚄𝚁𝙴 𝙲𝙰𝙽 𝙾𝙽𝙻𝚈 𝙱𝙴 𝚄𝚂𝙴𝙳 𝙸𝙽 𝙶𝚁𝙾𝚄𝙿𝚂. 🦇", 
+       private: "𝚈𝙾𝙾 𝚆𝚃𝙵, 𝙳𝙾 𝚈𝙾𝚄 𝚃𝙷𝙸𝙽𝙺 𝚈𝙾𝚄'𝚁𝙴 𝙳𝙾𝙸𝙽𝙶?🤨. 𝚃𝙷𝙸𝚂 𝙵𝙴𝙰𝚃𝚄𝚁𝙴 𝙲𝙰𝙽 𝙾𝙽𝙻𝚈 𝙱𝙴 𝚄𝚂𝙴𝙳 𝙸𝙽 𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙲𝙷𝙰𝚃. 🦇",
+       owner: "𝙿𝙸𝚂𝚂 𝙾𝙵𝙵 😑. 𝚃𝙷𝙸𝚂 𝙵𝙴𝙰𝚃𝚄𝚁𝙴 𝙲𝙰𝙽 𝙾𝙽𝙻𝚈 𝙱𝙴 𝚄𝚂𝙴𝙳 𝙱𝚈 𝙱𝙾𝚃 𝙾𝚆𝙽𝙴𝚁𝚂. 🦇",
+       admin: "𝚀𝚄𝙸𝚃 𝙼𝙴𝚂𝚂𝙸𝙽𝙶 𝙰𝚁𝙾𝚄𝙽𝙳😒. 𝚃𝙷𝙸𝚂 𝙵𝙴𝙰𝚃𝚄𝚁𝙴 𝙲𝙰𝙽 𝙾𝙽𝙻𝚈 𝙱𝙴 𝚄𝚂𝙴𝙳 𝙱𝚈 𝙱𝙾𝚃 𝙾𝚆𝙽𝙴𝚁𝚂. 🦇", 
+       badmin: "𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽 𝙵𝙸𝚁𝚂𝚃😐 𝚈𝙾𝚄 𝚂𝙴𝙻𝙵 𝙲𝙴𝙽𝚃𝙴𝚁𝙴𝙳 𝙲𝚄𝙽𝚃🤬. 🦇",
+       premium: "𝚀𝚄𝙸𝚃 𝙱𝙴𝙸𝙽𝙶 𝙰 𝙿𝚄𝚂𝚂𝙸𝙾 𝙰𝙽𝙳 𝙶𝙴𝚃 𝙿𝚁𝙴𝙼𝙸𝚄𝙼 𝙾𝚆𝙽𝙴𝚁𝚂𝙷𝙸𝙿😒. 🦇",
    }
 }
  
